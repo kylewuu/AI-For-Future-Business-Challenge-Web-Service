400bb92b7ed90ac48c3eaa3e2b330ffa9c3bd446
Kyle Wu <kylewu.1999@gmail.com>
2021-03-06 17:33:18 -0800

    rename api to app

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ from flask_cors import CORS, cross_origin
 companies = [{"id": 1, "name": "Company One"},
              {"id": 2, "name": "Company Two"}]
 
-api = Flask(__name__)
-cors = CORS(api)
-api.config['CORS_HEADERS'] = 'Content-Type'
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
-@api.route('/companies', methods=['GET'])
+@app.route('/companies', methods=['GET'])
 @cross_origin()
 def get_companies():
     return json.dumps(companies)
 
 
 if __name__ == '__main__':
-    api.run()
+    app.run()
