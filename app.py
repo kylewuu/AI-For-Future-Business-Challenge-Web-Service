2c478efb02d0729ed96f6a10f45d8b984bc80468
Kyle Wu <kylewu.1999@gmail.com>
2021-03-13 18:55:00 -0800

    undo revert

--- app.py
+++ app.py
@@ -1,55 +1,55 @@
-from flask import Flask, json
-from flask_cors import CORS, cross_origin
-
-companies = [{"id": 1, "health": "20%"},
-             {"id": 2, "health": "70%"}]
-
-app = Flask(__name__)
-cors = CORS(app)
-app.config['CORS_HEADERS'] = 'Content-Type'
-
-
-@app.route('/health', methods=['GET'])
-@cross_origin()
-def get_companies():
-    return json.dumps(companies)
-
-
-if __name__ == '__main__':
-    app.run()
-
-
-# from flask import Flask, json, request
+# from flask import Flask, json
 # from flask_cors import CORS, cross_origin
-# from functions.API.get_statuses import *
-# from functions.API.post_apples import *
-# from functions.initializations import *
-# import json
-# import os
 
+# companies = [{"id": 1, "health": "20%"},
+#              {"id": 2, "health": "70%"}]
 
 # app = Flask(__name__)
 # cors = CORS(app)
 # app.config['CORS_HEADERS'] = 'Content-Type'
-# detector = init()
 
 
-# @app.route('/status', methods=['GET'])
+# @app.route('/health', methods=['GET'])
 # @cross_origin()
-# def get_statuses():
-#     return json.dumps(get_statuses_all())
+# def get_companies():
+#     return json.dumps(companies)
 
-# @app.route('/status', methods=['POST'])
-# @cross_origin()
-# def post_apples():
-#     img_string = request.json['image']
-#     string_data = process_apples(detector, str(img_string))
-#     json_data = json.dumps(string_data)
-
-#     with open('resources\\temp_db\\data.json', 'w', encoding='utf-8') as f:
-#         json.dump(string_data, f, ensure_ascii=False, indent=4)
-
-#     return json_data
 
 # if __name__ == '__main__':
 #     app.run()
+
+
+from flask import Flask, json, request
+from flask_cors import CORS, cross_origin
+from functions.API.get_statuses import *
+from functions.API.post_apples import *
+from functions.initializations import *
+import json
+import os
+
+
+app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
+detector = init()
+
+
+@app.route('/status', methods=['GET'])
+@cross_origin()
+def get_statuses():
+    return json.dumps(get_statuses_all())
+
+@app.route('/status', methods=['POST'])
+@cross_origin()
+def post_apples():
+    img_string = request.json['image']
+    string_data = process_apples(detector, str(img_string))
+    json_data = json.dumps(string_data)
+
+    with open('resources\\temp_db\\data.json', 'w', encoding='utf-8') as f:
+        json.dump(string_data, f, ensure_ascii=False, indent=4)
+
+    return json_data
+
+if __name__ == '__main__':
+    app.run()
