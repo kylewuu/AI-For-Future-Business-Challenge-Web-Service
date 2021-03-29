bb587d05736cacfab616d282a9f69559f08c608c
Kyle Wu <kylewu.1999@gmail.com>
2021-03-28 18:00:39 -0700

    remove comments

--- app.py
+++ app.py
@@ -1,24 +1,3 @@
-# from flask import Flask, json
-# from flask_cors import CORS, cross_origin
-
-# companies = [{"id": 1, "health": "20%"},
-#              {"id": 2, "health": "70%"}]
-
-# app = Flask(__name__)
-# cors = CORS(app)
-# app.config['CORS_HEADERS'] = 'Content-Type'
-
-
-# @app.route('/health', methods=['GET'])
-# @cross_origin()
-# def get_companies():
-#     return json.dumps(companies)
-
-
-# if __name__ == '__main__':
-#     app.run()
-
-
 from flask import Flask, json, request
 from flask_cors import CORS, cross_origin
 from functions.API.get_statuses import *
